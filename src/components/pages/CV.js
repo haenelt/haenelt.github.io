39da5c03fb995762bc6522d64cda6e58cecb6272
haenelt <haenelt@cbs.mpg.de>
2022-02-17 14:55:30 +0100

    make home styles mobile

--- src/components/pages/CV.js
+++ src/components/pages/CV.js
@@ -1,11 +1,16 @@
 import pdfFile from '../../assets/cv_2022_02.pdf';
+import styles from './CV.module.css';
 
 const CV = () => {
   return (
     <div >
       <h1>Curriculum Vitae</h1>
-      <object data={pdfFile} type="application/pdf" width="100%" height="800px"><p>Alternative text - include a link <a href={pdfFile}>to the PDF!</a></p></object>
-    </div>
+      <div className={styles.pdf}>
+        <object data={pdfFile + "#toolbar=1&navpanes=0&scrollbar=1&page=1&view=FitW"} type="application/pdf">
+          <p>You do not seem to have a functional PDF plug-in for this browser. <a href={pdfFile}>Link to the PDF!</a></p>
+        </object>
+      </div>
+    </div >
   );
 };
 
