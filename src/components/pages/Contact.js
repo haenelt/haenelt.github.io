d49d02d3bb3bad94523df023c4c420248a4798da
haenelt <haenelt@cbs.mpg.de>
2024-02-09 23:05:48 -0500

    add contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,9 +4,8 @@ const Contact = () => {
   return (
     <div>
       <h1>Contact</h1>
-      <p className={styles.item}><strong>Mail</strong>: <a href="mailto:daniel.haenelt@gmail.com">daniel.haenelt@gmail.com</a> (personal) / <a href="mailto:haenelt@cbs.mpg.de">haenelt@cbs.mpg.de</a> (work)</p>
-      <p className={styles.item}><strong>Phone</strong>: <a href="tel:004934199402438">+49 (0) 341 9940 2438</a></p>
-      <p className={styles.item}><strong>Address</strong>: Max Planck Institute for Human Cognitive and Brain Sciences, Stephanstraße 1a, 04103 Leipzig, Germany</p>
+      <p className={styles.item}><strong>Mail</strong>: <a href="mailto:daniel.haenelt@gmail.com">daniel.haenelt@gmail.com</a> (personal) / <a href="mailto:dhaenelt@mgh.harvard.edu">dhaenelt@mgh.harvard.edu</a> (work)</p>
+      <p className={styles.item}><strong>Address</strong>: Athinoula A. Martinos Center for Biomedical Imaging, 149 Thirteenth Street, Suite 2301, Charlestown, MA 02129 USA</p>
     </div>
   );
 };
