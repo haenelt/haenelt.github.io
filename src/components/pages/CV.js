04afbf50c7c62dba31ad191f878493c30026f3e6
Daniel Haenelt <daniel.haenelt@gmail.com>
2026-06-11 14:13:50 -0400

    add new publications

--- src/components/pages/CV.js
+++ src/components/pages/CV.js
@@ -1,7 +1,7 @@
 import styles from './CV.module.css';
 
 const CV = () => {
-  const cvFile = "https://drive.google.com/file/d/1U0-9VpiOtNnar1pVSynfBZyzNEWmOOIH/preview";
+  const cvFile = "https://drive.google.com/file/d/1v61neg4DDmL7XFQ1YoW-G2WiErSizs5F/preview";
 
   return (
     <div >
